27bb850ddc4dcaa3a9ff848338be241d81ca3aa9
edgarfelizmenio <edgarfelizmenio@gmail.com>
2014-10-06 16:02:12 +0800

    removed performance bottlenecks from base template

--- application/views/templates/base.php
+++ application/views/templates/base.php
@@ -1,47 +1,6 @@
 <!DOCTYPE html>
-<!--
-        ,i@@@G;::::::,,:,,,,,,..,,,,,.,;f@@@@@@@@@@@@80GG00@@@@@@@@8LCGf;.     .:tfLG@@@@@@@@@@@8@8@@@@Ltf@@@@@@@.         ;;;.           .,,. .   ,fC;  t,   :t
-        ,;L@@@Gftt11111iii;;;;::::,,,,,:i@@@@@@@@@8GCG@@@@@@0GGGCfttfff1i;:   .;tft1111iiii;;;::;;1tL08Gt1L@@@@@:       .;;:            ,:,,,....   it;  .L    1
-     8f .:1@@@@@@@@@@@@@@@@@@@@@@@@@@@0C8@@@@@@@@8GC8@@@0t@@@@@@0CLi;i11i;:.  .,:,,.,:1fC@@@@tLGCtii;itf11t@@@@t.     :ii;          .::....,,.      ,ti.  C    :
-     GC   .           ,:f0@@@@@@@@@@@@@@@@@@@@880008@@@01t@@.i@1 if;:;1tti,     .,..,ii:0@@@01  .08G1i;;;it@@@8i.  .;ii:           :;::..::,.       .i1:. @    :
-    G@.                                 t@@@@@8000088@@@t.:@@@   iLC itfti,         :t: .@@@@@  ,fGL1;:::;1@@@t: .;1i:          ..,:1:,...,..        ,;,. f;   .
-                                        8@@@8800GGGG00880GCfi;,,;1t1:1LL1i:..      .iffti::f1  :tf1;:,.,::i@@Gi::1i,         .;,...,tf,....          .:;,  L    
-                                        :@@@88880GCLLLLftt1:..   .;;1tLLti:,.  ..    .,,;i1111i;;:....,,:;i@@t:,:,        .:i1;,....1G.               ,;i:.1. C;
-          :::;:,,,,,..                   @@@@80GGCftttt1i;:.    .,,;1tftti:.    ..           ..      ..,:;1@@i.         ,:;11ii,..  iC:                :i;,;8 t.
-                                ...,,,:,:@@@80GCLtt1ii;;:,,,.   .,;i1ttt1i:..                         .,:;1@G;.      :ii;;;11ii,.   iLi                 @@G,@ ;:
-                                         i@@@GCLftt1;::,:,,,,....,;itLLfti:,.    .                     .:;f@@;    ,i1t1ii:;11ii,    ;L1            .,.,,1@1 @C :
-                                          @@@0Cfft1i;:,,,,....   .;tCCft1;,.      ..                   .,i0@t:.:1t1tfft1;:;11;:,..  ;fi     ...,;:tG;,,::L8tC ;1
-                                          @@@8CLftii;;:,,...     .i00Cfti:,.     ..,.                 ..:i@@;.,,;1tffff1;::;;:,,.   :t1...,.,..,i,,:, .,;18Ci.. 
-                                          8@@@GLf1ii;::,.       .i00Lti::,.        .:,                .,:G@G:   :1tfffft;,,::,,.  .LCCf..C0f:   :,,:::1C88G1i   
-                                           t@@8CLt11;:,,.       ,ffi,..,.            ,               ..,i@@i. ..:11fft1i;,,,:,... .C@@@:,t:.     ,,;:.,:iiiii,  
-                                            @@@0Ct11;:,.        :ti,,:tt:.     ,i:                   .,:f@@,.   ,11111ii:,,,.......f@@f;ff;. ..:1::;;,,:ii1i1i. 
-fft1.                                       @@@8CLt1i:,.        :11i;iii1t:...   ,:,.                .,;@@@;    .i1iii;;:,,,,,;Lt:.i88Litf:...,:,:;i;,,,i1ii1f, 
-L80@0;                                      8@@@GLf11;:,.      ,;ii1tffLCt, ,;:,...                 ..:f@@f,     ;iii;;;;,:fGf:i;,   i1if8:,,:;;;::;i;,;i111it: 
-G000@8                                       G@@@Cft1i:,,    ,:;iitL08880f;:tG00f1:.                .,1@@@,.     ;L08@L11;t1i:,i;;:i::i;::,:::;;;;;iii::i1111t1,
-f888@@t                                       @@@0Lf1i;:,,,,;iitfG8@@@@@8Li;t088@@0f1i,            .,1C@@8:   .:0@@@@@888Cfti;;f1;;;:;;i;:,,,:;;;;:i1i;:;1111tf:
-800G;@8                                       :@@@8Lt1i;:;1L0@@@@@@@@@Lt;,:;;i1fG@@@@@@8Cti.    ...,1C@@@1.. .t@@@@@@C8@888Lttt1ti;;;;i;:;:,:;;iiii111i:;i1111fi
-000;.@@                                        t@@@8Lt1iif@@@@@@@@@@@000CLi,;fCG888@@@@@@@@Li....,,;f@@@@i...t@@0@@@@GCCG888@Cf11ii;;;;i;::::;iiiii11t1;;;;ii;i1
-000,,@@                                         8@@@@LfttG@@@@@@@@@@@@8000CLCCG088@@@@@@@@@@C;,,:;1G@@@G1,..;@@f0@@@@@CLLL888@Gftii;;iiiii;;:;;;;;:;;i1i;ii11iit
-000i:@@                          .               G@@@8CLfG@@@@@@0G088800CCLCCCLLCCCGG0@@@@@@L;:;iif@@@@1:..,8@fLL@@@@@GLC@@@@@@Cft1iiii11i;;iiii;;;;i11i;ii1i11t
-80f18@@                    iG8@@@@@@@@@@@@@8888@@@@@@@@0CC8@@@GLG8@@@0Li:::;:,,,;tLGLCC0@@@@G1;i1C@@@@;,...;@@Lt1@@@@@GC@@GL88@8ft11i11111i;;;iii;;ii1t1iiii1111
-00L8i@8                  t0@,                 :fG@@@@@@@800@@0CfC008880Li;::;i1tLLCCCLtf@@80Lt1f0@@@01:,,.:0@@Lt1@@@@@GC@Cff@@G@CLt111i1t1i1iiiiiii;i1ttiii1t11i
-08GL1@@                 888               :tC8@@@@@@@@@@@@@80CLfLCG8@@@8088880G8088CttftfG00CC8@@@@G1ii;;::@@88Lt8@@@@0C@@@@@@8@@Cft111ttt1iiiiii11i1tftt111t1t1
-8CC08@@                ;88            ;fC8@@@88888@@@@@@@@@@@8GLLLCG08@@@@@@@@880CCCftttfC@@@@@@@@C1;::;iiiii1t@fL888@@C@@80LL8@@@Lfttt1t11ii111i1t1111ttt1tftff
-0GLit@8                @8C       .;fC0@88888000008@@@@@@@@@@@@8GCLLC088@@@@@@8LCLCCCGGCG8@@@@@@@@@1;:,,::;ii1iii;i1L8@@0@8LftfG@@@0Lfttfft11111t1ttfLLCCfttfCCCC
-08tiG@L.               @8:   :1LG@@8888800GCCCCCGG8@@@@@@@@@@@@@888@@@@@@@@@@@@088@@@@8@@@@@@@@@0i:...,.,::;;;;;iii;;iiitCfftfL@@@@CLffffft1ttttLffLLLCCLttLCCGG
-
-              d888888b Cb .88b  d88.      db   d8b   db  .d8b.  d888888b  .o88b. db   db d888888b d8b   db  d888b       db    db  .d88b.  db    db 
-                `88'   `D 88'YbdP`88      88   I8I   88 d8' `8b `~~88~~' d8P  Y8 88   88   `88'   888o  88 88' Y8b      `8b  d8' .8P  Y8. 88    88 
-                 88     ' 88  88  88      88   I8I   88 88ooo88    88    8P      88ooo88    88    88V8o 88 88            `8bd8'  88    88 88    88 
-                 88       88  88  88      Y8   I8I   88 88~~~88    88    8b      88~~~88    88    88 V8o88 88  ooo         88    88    88 88    88 
-                .88.      88  88  88      `8b d8'8b d8' 88   88    88    Y8b  d8 88   88   .88.   88  V888 88. ~8~         88    `8b  d8' 88b  d88 
-              Y888888P    YP  YP  YP       `8b8' `8d8'  YP   YP    YP     `Y88P' YP   YP Y888888P VP   V8P  Y888P          YP     `Y88P'  ~Y8888P' 
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
--->
 <html>
   <head>
-
-    <link href='http://fonts.googleapis.com/css?family=Dosis|Oswald:400,300|Open+Sans:400,700|Roboto:400,300' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" type="text/css"
       href="<?php echo base_url('/packaged/css/semantic.css'); ?>">
     <link rel="stylesheet" type="text/css"
