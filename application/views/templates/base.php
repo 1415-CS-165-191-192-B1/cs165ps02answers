2f5112f40c9f32831520486e890f250ca4a48f45
edgarfelizmenio <edgarfelizmenio@gmail.com>
2014-10-06 16:11:21 +0800

    moved script tags from head to bottom

--- application/views/templates/base.php
+++ application/views/templates/base.php
@@ -12,20 +12,6 @@
       href="<?php echo base_url('/packaged/jquery-ui-1.11.1.custom/jquery-ui.structure.min.css'); ?>">
     <link rel="stylesheet" type="text/css"
       href="<?php echo base_url('/packaged/jquery-ui-1.11.1.custom/jquery-ui.theme.min.css'); ?>">
-
-
-    <script src=
-      "<?php echo base_url('/packaged/javascript/jquery-1.10.2.min.js'); ?>">
-    </script>
-    <script src=
-      "<?php echo base_url('/packaged/jquery-ui-1.11.1.custom/jquery-ui.min.js'); ?>">
-    </script>
-
-    <script src="<?php echo base_url('/packaged/javascript/semantic.js'); ?>">
-    </script>
-    <script src="<?php echo base_url('/packaged/javascript/ui.js'); ?>">
-    </script>
-
     <title><?php echo $title; ?> - CS 165 Problem Set 2</title>
 
   </head>
@@ -44,4 +30,17 @@
         <?php echo $body; ?>
       </div>
   </body>
+
+  <script src=
+    "<?php echo base_url('/packaged/javascript/jquery-1.10.2.min.js'); ?>">
+  </script>
+  <script src=
+    "<?php echo base_url('/packaged/jquery-ui-1.11.1.custom/jquery-ui.min.js'); ?>">
+  </script>
+
+  <script src="<?php echo base_url('/packaged/javascript/semantic.js'); ?>">
+  </script>
+  <script src="<?php echo base_url('/packaged/javascript/ui.js'); ?>">
+  </script>
+
 </html>
